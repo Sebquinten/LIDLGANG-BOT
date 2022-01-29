641cc841d2179690bec2284b19ea0174bd216f98
Sebquinten <sebastquinten@gmail.com>
2022-01-29 14:55:40 +0100

    part 2

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS"
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -23,6 +26,16 @@ client.on('messageCreate', async message => {
     if (message.content.includes("lvo") ){
         await message.react("<:sheeshVO:870554241079586877>")
     }
+})
+
+const welcomeChannelId = "936641026532188170"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content:`<@${member.id}> Welcome to LIDLGANG!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
