bed6359c6be7fce8e130df46ef88c7fb1f6f7b0d
Sebquinten <sebastquinten@gmail.com>
2022-02-06 21:08:35 +0100

    lidlgang command

---
+++ commands/info/lidlmembers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "lidlgang",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("**LIDLGANG members:**\nnwil\nBedumb\nBentoski\nVarna\n_LVO\nTemperative\nSnow999\nShikado\nShemmy\nOnblij\nllogiic")
+    }
+} 
